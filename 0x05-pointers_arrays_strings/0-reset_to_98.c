7c58c91b76828db82685d4c8393856187c6027e1
Darleen10 <101571885+Darleen10@users.noreply.github.com>
2022-07-12 02:14:29 +0200

    My first c pointers commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Oluwadamilola Ajibuwa
+ */
+
+#include <stdio.h>
+void reset_to_98(int *n);
+{
+    *var = *var+b;
+}
+int main()
+{
+   int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
+}
