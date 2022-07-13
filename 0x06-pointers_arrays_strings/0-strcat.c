d7b96b33c9dd12ea77bceb1df83130b043b75ca9
Your Name <you@example.com>
2022-07-13 11:23:00 -0700

    Created the 0-strcat file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strncat(char *dest, char *src)
+{
+	int i,j;
+	for(i = 0; dest[i] != '\0'; i++);
+	for(j = 0; src[j] != '\0'; j++) {
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return(dest);
+}
+
